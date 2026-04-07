678add12862b39599f8f0afece2ce7b21f66e552
muhammed arshad <arshadkorlotty@gmail.com>
2026-04-07 10:06:37 +0530

    feat: implement product listing  and  home view navbar

--- user_apps/core/views.py
+++ user_apps/core/views.py
@@ -13,9 +13,14 @@ def landing_view(request):
     
     context = {'products': products}
     if request.user.is_authenticated:
-        from user_apps.core.models import WishlistItem
+        from user_apps.core.models import WishlistItem, CartItem
+        from django.db.models import Sum
         wishlist_product_ids = WishlistItem.objects.filter(wishlist__user=request.user).values_list('product_id', flat=True)
         context['wishlist_product_ids'] = list(wishlist_product_ids)
+        
+        # Add cart count
+        cart_count = CartItem.objects.filter(cart__user=request.user).aggregate(Sum('quantity'))['quantity__sum'] or 0
+        context['cart_count'] = cart_count
         
     return render(request, "core/landing.html", context)
 
@@ -27,8 +32,13 @@ def home_view(request):
     
     context = {'products': products}
     if request.user.is_authenticated:
-        from user_apps.core.models import WishlistItem
+        from user_apps.core.models import WishlistItem, CartItem
+        from django.db.models import Sum
         wishlist_product_ids = WishlistItem.objects.filter(wishlist__user=request.user).values_list('product_id', flat=True)
         context['wishlist_product_ids'] = list(wishlist_product_ids)
+        
+        # Add cart count
+        cart_count = CartItem.objects.filter(cart__user=request.user).aggregate(Sum('quantity'))['quantity__sum'] or 0
+        context['cart_count'] = cart_count
         
     return render(request, 'core/home.html', context)
