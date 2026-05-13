5e5e3ad0e7d01c68e8b0119b339e8adb5eacf1b1
arshhaad <arshadkorlotty@gmail.com>
2026-05-13 16:08:02 +0530

    fix: remove unused configuration file and associated dependencies

--- admin_apps/auth/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthConfig(AppConfig):
-    name = 'admin_apps.auth'
-    default_auto_field = 'django.db.models.BigAutoField'
-    label = 'admin_auth'
-    
--- admin_apps/offers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OffersConfig(AppConfig):
-    name = 'admin_apps.offers'
