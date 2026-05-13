a7a21ae7fe6ef20b82207134a06a9310f4308d96
arshhaad <arshadkorlotty@gmail.com>
2026-05-13 16:18:21 +0530

    feat: implement authentication app including base models, views, templates, and dashboard interface

---
+++ admin_apps/auth/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
